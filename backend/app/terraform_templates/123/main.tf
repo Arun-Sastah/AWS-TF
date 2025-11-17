3750fd334f29289444656a94fc2ec881645fac15
Arun-Sastah <arunaachalam.uthirapathy@sastah.com>
2025-11-17 16:30:07 +0530

    Fixed path issues

---
+++ backend/app/terraform_templates/123/main.tf
@@ -0,0 +1,7 @@
+module "ec2" {
+  source        = "../../modules/ec2"
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+  instance_name = "test-instance"
+  device_id     = "123"
+}
---
+++ backend/app/terraform_templates/123/variables.tf
@@ -0,0 +1,9 @@
+variable "device_id" {
+  type        = string
+  description = "Unique device ID"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "EC2 instance name"
+}
